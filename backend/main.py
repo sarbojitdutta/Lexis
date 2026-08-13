01d9ab0d43c19c70ab4b13a1d64675422baad6fb
sarbojitdutta <sarbojitd48@gmail.com>
2026-08-13 17:54:33 +0530

    small update in backend

--- backend/main.py
+++ backend/main.py
@@ -16,6 +16,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins     = [
+        "https://lexis-puce.vercel.app/"
         "http://localhost:3000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
